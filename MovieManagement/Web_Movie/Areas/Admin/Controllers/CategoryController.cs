020268715c028caebf2b19ab5457637a7ac291e7
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-01-30 20:54:47 +0700

    Hoàn thành thêm, xóa, sửa Category

--- MovieManagement/Web_Movie/Areas/Admin/Controllers/CategoryController.cs
+++ MovieManagement/Web_Movie/Areas/Admin/Controllers/CategoryController.cs
@@ -18,6 +18,7 @@ namespace Web_Movie.Areas.Admin.Controllers
                 ? _context.Categories.Where(c => c.ParentId == null).ToList()  // Danh mục gốc
                 : _context.Categories.Where(c => c.ParentId == parentId).ToList();  // Danh mục con của parentId
 
+            ViewBag.ParentId = parentId;
             // Trả về danh sách categories cho view
             return View(categories);
         }
@@ -36,6 +37,71 @@ namespace Web_Movie.Areas.Admin.Controllers
                 }).ToListAsync();
 
             return Json(subCategories);
+        }
+        public IActionResult Create(Guid? parentId)
+        {
+            if (parentId.HasValue)
+            {
+                ViewData["ParentId"] = parentId.Value;
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Guid? parentId, Category category)
+        {
+            var categoryModel = new Category();
+            categoryModel.CategoryId = Guid.NewGuid();
+            categoryModel.CategoryName = category.CategoryName;
+            categoryModel.Status = category.Status;
+            categoryModel.CreatedDate = DateTime.Now;
+            if (parentId == Guid.Empty)
+                categoryModel.ParentId = null;
+            categoryModel.ParentId = parentId;
+            _context.Categories.Add(categoryModel);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Thêm thành công";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(Guid? parentId, Guid id)
+        {
+            var categoryExist = _context.Categories.Find(id);
+            if (parentId.HasValue)
+            {
+                ViewData["ParentId"] = parentId.Value;
+            }
+            return View(categoryExist);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid? parentId, Guid id, Category category)
+        {
+            var categoryExist = _context.Categories.Find(id);
+            if (categoryExist == null)
+            {
+                return NotFound();
+            }
+            categoryExist.CategoryName = category.CategoryName;
+            categoryExist.Status = category.Status;
+            categoryExist.CreatedDate = DateTime.Now;
+            if (parentId == Guid.Empty)
+                categoryExist.ParentId = null;
+            categoryExist.ParentId = parentId;
+            _context.Categories.Update(categoryExist);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Cập nhật thành công";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var categoryExist=_context.Categories.Find(id);
+            if(categoryExist == null)
+            {
+                return NotFound();
+            }    
+            _context.Categories.Remove(categoryExist);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Xóa thành công";
+            return Redirect(Request.Headers["Referer"]); //quay lại trang ngay trước đó
         }
     }
 }
