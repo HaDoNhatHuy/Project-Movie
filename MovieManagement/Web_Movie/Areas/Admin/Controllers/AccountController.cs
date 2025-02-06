f4fb88dfc117f967de3e19f85b8163e3ccbd2a5e
Hà Đỗ Nhật Huy <huyhd.22th@sv.daihoclongan.edu.vn>
2025-02-06 12:10:40 +0700

    6/2/2025: Hoàn thành phần account Admin và cập nhật các thông tin và thay đổi mật khẩu

--- MovieManagement/Web_Movie/Areas/Admin/Controllers/AccountController.cs
+++ MovieManagement/Web_Movie/Areas/Admin/Controllers/AccountController.cs
@@ -15,11 +15,13 @@ namespace Web_Movie.Areas.Admin.Controllers
         private UserManager<AppUserModel> _userManager;
         private SignInManager<AppUserModel> _signInManager;
         private readonly MovieContext _context;
-        public AccountController(UserManager<AppUserModel> userManager, SignInManager<AppUserModel> signInManager, MovieContext context)
+        private IWebHostEnvironment _webHostEnvironment;
+        public AccountController(UserManager<AppUserModel> userManager, SignInManager<AppUserModel> signInManager, MovieContext context, IWebHostEnvironment webHostEnvironment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Account(string returnUrl)
         {
@@ -43,7 +45,7 @@ namespace Web_Movie.Areas.Admin.Controllers
             }
             var result = await _signInManager.PasswordSignInAsync(user.UserName, loginVM.LoginViewModel.Password, false, false);
             if (result.Succeeded)
-            {                
+            {
                 return Redirect(loginVM.LoginViewModel.ReturnUrl ?? "/Admin");
             }
             return View("Account", new AccountViewModel
@@ -80,13 +82,85 @@ namespace Web_Movie.Areas.Admin.Controllers
             await _signInManager.SignOutAsync();
             return Redirect(returnUrl);
         }
-        public IActionResult MyAccount()
+        public async Task<IActionResult> MyAccount()
         {
+            var currentUser = await _userManager.GetUserAsync(User);
+            ViewBag.CurrentUser = currentUser;
             return View();
         }
-        public IActionResult Settings()
+        public async Task<IActionResult> Settings()
         {
+            var currentUser = await _userManager.GetUserAsync(User);
+            ViewBag.CurrentUser = currentUser;
             return View();
+        }
+        public async Task<IActionResult> UpdateProfile(AppUserModel userModel, IFormFile Avatar)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            var updateUser = await _userManager.FindByEmailAsync(currentUser.Email);
+            if (updateUser == null)
+            {
+                return NotFound();
+            }
+            updateUser.FullName = userModel.FullName;
+            updateUser.Avatar = userModel.Avatar;
+            updateUser.Address = userModel.Address;
+            updateUser.PhoneNumber = userModel.PhoneNumber;
+            if (Avatar != null)
+            {
+                var path = Path.Combine(this._webHostEnvironment.WebRootPath, "assets/admin/images/UserImages/", Avatar.FileName);//lấy đường dẫn hình
+                                                                                                                                  //lưu hình
+                using (FileStream stream = new FileStream(path, FileMode.Create))
+                {
+                    await Avatar.CopyToAsync(stream); // copy vào đường dẫn stream
+                    stream.Close();
+                }
+                updateUser.Avatar = Avatar.FileName;
+            }
+            //Kiểm tra mk hiện tại nếu cần thay đổi mk
+            if (!string.IsNullOrEmpty(userModel.PasswordHash))
+            {
+                var passwordHasher = new PasswordHasher<IdentityUser>();
+
+                //kiểm tra mk hiện tại
+                var passwordVerificationResult = passwordHasher.VerifyHashedPassword(currentUser, currentUser.PasswordHash, userModel.PasswordHash);
+                if (passwordVerificationResult == PasswordVerificationResult.Failed)
+                {
+                    TempData["error"] = "Mật khẩu hiện tại không chính xác";
+                    return RedirectToAction("Settings", "Account");
+                }
+                //kiểm tra mk mới và xác nhận mk mới
+                if (!string.IsNullOrEmpty(userModel.NewPassword) && !string.IsNullOrEmpty(userModel.ConfirmPassword))
+                {
+                    if (userModel.NewPassword == userModel.PasswordHash)
+                    {
+                        TempData["error"] = "Mật khẩu mới không được trùng với mật khẩu hiện tại.";
+                        return RedirectToAction("Settings", "Account");
+                    }
+                    if (userModel.NewPassword != userModel.ConfirmPassword)
+                    {
+                        TempData["error"] = "Xác nhận mật khẩu không khớp với mật khẩu mới.";
+                        return RedirectToAction("Settings", "Account");
+                    }
+                    currentUser.PasswordHash = passwordHasher.HashPassword(currentUser, userModel.NewPassword);
+                }
+                else
+                {
+                    TempData["error"] = "Mật khẩu mới và xác nhận mật khẩu không được để trống.";
+                    return RedirectToAction("Settings", "Account");
+                }
+            }
+            // Cập nhật thông tin người dùng
+            var result = await _userManager.UpdateAsync(currentUser);
+            if (!result.Succeeded)
+            {
+                TempData["error"] = "Có lỗi xảy ra khi cập nhật tài khoản.";
+                return RedirectToAction("Settings", "Account");
+            }            
+            await _userManager.UpdateAsync(updateUser);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Cập nhật tài khoản thành công";
+            return RedirectToAction("Settings", "Account");
         }
     }
 }
